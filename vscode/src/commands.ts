a87c73402f9ca7647c9c7feb47c2ab4d1ae5b7ba
Ian Bolton <ibolton@redhat.com>
2024-09-27 16:51:21 -0400

    :sparkles: Add extension state and analysis placeholders (#29)
    - Add placeholder menu items for analyzing specific files from the
    explorer view
    - Add placeholder startAnalysis command 
    - Create utility function for getting the webview content since it is
    being used in panel and sidebar webviews
    - Create extension state to hold any new webview providers. Allows
    access to the state without having to change function signatures to pass
    in any new webviews to the registerCommand function as they are created.
    
    Signed-off-by: Ian Bolton <ibolton@redhat.com>

--- vscode/src/commands.ts
+++ vscode/src/commands.ts
@@ -1,6 +1,8 @@
 import * as vscode from "vscode";
 import { KonveyorGUIWebviewViewProvider } from "./KonveyorGUIWebviewViewProvider";
 import { setupWebviewMessageListener } from "./webviewMessageHandler";
+import { ExtensionState } from "./extensionState";
+import { getWebviewContent } from "./webviewContent";
 
 let fullScreenPanel: vscode.WebviewPanel | undefined;
 
@@ -13,12 +15,39 @@ function getFullScreenTab() {
 
 const commandsMap: (
   extensionContext: vscode.ExtensionContext,
-  sidebar: KonveyorGUIWebviewViewProvider,
-) => { [command: string]: (...args: any) => any } = (
-  extensionContext,
-  sidebar,
+  state: ExtensionState
 ) => {
+  [command: string]: (...args: any) => any;
+} = (extensionContext, state) => {
+  const { sidebarProvider } = state;
   return {
+    "konveyor.startAnalysis": async (resource: vscode.Uri) => {
+      if (!resource) {
+        vscode.window.showErrorMessage("No file selected for analysis.");
+        return;
+      }
+
+      // Get the file path
+      const filePath = resource.fsPath;
+
+      // Perform your analysis logic here
+      try {
+        // For example, read the file content
+        const fileContent = await vscode.workspace.fs.readFile(resource);
+        const contentString = Buffer.from(fileContent).toString("utf8");
+
+        console.log(contentString, fileContent);
+
+        // TODO: Analyze the file content
+        vscode.window.showInformationMessage(`Analyzing file: ${filePath}`);
+
+        // Call your analysis function/module
+        // analyzeFileContent(contentString);
+      } catch (error) {
+        vscode.window.showErrorMessage(`Failed to analyze file: ${error}`);
+      }
+    },
+
     "konveyor.focusKonveyorInput": async () => {
       const fullScreenTab = getFullScreenTab();
       if (!fullScreenTab) {
@@ -28,14 +57,14 @@ const commandsMap: (
         // focus fullscreen
         fullScreenPanel?.reveal();
       }
-      // sidebar.webviewProtocol?.request("focusContinueInput", undefined);
+      // sidebar.webviewProtocol?.request("focusInput", undefined);
       // await addHighlightedCodeToContext(sidebar.webviewProtocol);
     },
     "konveyor.toggleFullScreen": () => {
       // Check if full screen is already open by checking open tabs
       const fullScreenTab = getFullScreenTab();
 
-      // Check if the active editor is the Continue GUI View
+      // Check if the active editor is the GUI View
       if (fullScreenTab && fullScreenTab.isActive) {
         //Full screen open and focused - close it
         vscode.commands.executeCommand("workbench.action.closeActiveEditor"); //this will trigger the onDidDispose listener below
@@ -61,10 +90,10 @@ const commandsMap: (
       fullScreenPanel = panel;
 
       //Add content to the panel
-      panel.webview.html = sidebar.getSidebarContent(
+      panel.webview.html = getWebviewContent(
         extensionContext,
-        panel,
-        true,
+        sidebarProvider?.webview || panel.webview,
+        true
       );
 
       setupWebviewMessageListener(panel.webview);
@@ -83,10 +112,10 @@ const commandsMap: (
 
 export function registerAllCommands(
   context: vscode.ExtensionContext,
-  sidebar: KonveyorGUIWebviewViewProvider
+  state: ExtensionState
 ) {
   for (const [command, callback] of Object.entries(
-    commandsMap(context, sidebar)
+    commandsMap(context, state)
   )) {
     context.subscriptions.push(
       vscode.commands.registerCommand(command, callback),
