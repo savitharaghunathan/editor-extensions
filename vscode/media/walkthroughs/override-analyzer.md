3602974cd938c80b1f0879f7eb34b908502f6176
David Zager <dzager@redhat.com>
2024-09-27 17:43:36 -0400

    :sparkles: Extension Configuration (#32)
    This PR makes the essential bits of our extension configurable in a
    VSCode native way. We now provide:
    1. Configuration, which means the user can modify the configuration like
    any other extension in the application settings view.
    1. Walkthroughs, that step the user through the different things that
    can be configured and how.
    1. Some basic commands for configuring in a more hand-holding kind of
    way (filepicker for analyzer, picker for sources/targets, dialog to
    useDefaultRulesets).
    
    We leave the runAnalyzer piece unimplemented for now as that will come
    when it actually exists.
    
    Fixes #4
    Fixes #5
    
    Signed-off-by: David Zager <dzager@redhat.com>

---
+++ vscode/media/walkthroughs/override-analyzer.md
@@ -0,0 +1,7 @@
+Override Analyzer Binary
+========================
+
+The Konveyor extension comes packaged with a default analyzer binary. However,
+you may want to use a custom or updated version of the analyzer.
+
+To override the default analyzer run the [Konveyor: Override Analyzer Binaries](command:konveyor.overrideAnalyzerBinaries) command.
--- vscode/src/commands.ts
+++ vscode/src/commands.ts
@@ -3,6 +3,7 @@ import { KonveyorGUIWebviewViewProvider } from "./KonveyorGUIWebviewViewProvider
 import { setupWebviewMessageListener } from "./webviewMessageHandler";
 import { ExtensionState } from "./extensionState";
 import { getWebviewContent } from "./webviewContent";
+import { sourceOptions, targetOptions } from "./config/labels";
 
 let fullScreenPanel: vscode.WebviewPanel | undefined;
 
@@ -106,6 +107,177 @@ const commandsMap: (
         null,
         extensionContext.subscriptions,
       );
+    },
+    "konveyor.overrideAnalyzerBinaries": async () => {
+      const options: vscode.OpenDialogOptions = {
+        canSelectMany: false,
+        openLabel: 'Select Analyzer Binary',
+        filters: {
+          'Executable Files': ['exe', 'sh', 'bat', ''],
+          'All Files': ['*']
+        }
+      };
+
+      const fileUri = await vscode.window.showOpenDialog(options);
+      
+      if (fileUri && fileUri[0]) {
+        const filePath = fileUri[0].fsPath;
+        
+        // Update the user settings
+        const config = vscode.workspace.getConfiguration('konveyor');
+        await config.update('analyzerPath', filePath, vscode.ConfigurationTarget.Global);
+        
+        vscode.window.showInformationMessage(`Analyzer binary path updated to: ${filePath}`);
+      } else {
+        vscode.window.showInformationMessage('No analyzer binary selected.');
+      }
+    },
+    "konveyor.configureCustomRules": async () => {
+      const options: vscode.OpenDialogOptions = {
+        canSelectMany: true,
+        canSelectFolders: true,
+        canSelectFiles: true,
+        openLabel: 'Select Custom Rules',
+        filters: {
+          'All Files': ['*']
+        }
+      };
+
+      const fileUris = await vscode.window.showOpenDialog(options);
+      
+      if (fileUris && fileUris.length > 0) {
+        const customRules = fileUris.map((uri) => uri.fsPath);
+        
+        // TODO(djzager): Should we verify the rules provided are valid?
+
+        // Update the user settings
+        const config = vscode.workspace.getConfiguration('konveyor');
+        await config.update('customRules', customRules, vscode.ConfigurationTarget.Workspace);
+
+        // Ask the user if they want to disable the default ruleset
+        const useDefaultRulesets = await vscode.window.showQuickPick(
+          ['Yes', 'No'],
+          {
+            placeHolder: 'Do you want to use the default rulesets?',
+            canPickMany: false
+          }
+        );
+
+        if (useDefaultRulesets === 'Yes') {
+          await config.update('useDefaultRulesets', true, vscode.ConfigurationTarget.Workspace);
+        } else if (useDefaultRulesets === 'No') {
+          await config.update('useDefaultRulesets', false, vscode.ConfigurationTarget.Workspace);
+        }
+        
+        vscode.window.showInformationMessage(`Custom Rules Updated: ${customRules}\nUse Default Rulesets: ${useDefaultRulesets}`);
+      } else {
+        vscode.window.showInformationMessage('No custom rules selected.');
+      }
+    },
+    "konveyor.configureSourcesTargets": async() => {
+      const config = vscode.workspace.getConfiguration('konveyor');
+      const currentLabelSelector = config.get<string>('labelSelector', '');
+
+      // Function to extract values from label selector
+      const extractValuesFromSelector = (selector: string, key: string): string[] => {
+        const regex = new RegExp(`konveyor.io/${key}=(.*?)(?:\\s|$)`, 'g');
+        const matches = selector.matchAll(regex);
+        const values = Array.from(matches, match => match[1]);
+        return values.flatMap(value => value.split('|'));
+      };
+
+      // Extract sources and targets from the current label selector
+      const currentSources = extractValuesFromSelector(currentLabelSelector, 'source');
+      const currentTargets = extractValuesFromSelector(currentLabelSelector, 'target');
+
+      const state: { sources: string[]; targets: string[]; labelSelector: string } = { sources: [], targets: [], labelSelector: '' };
+
+      // Function to show QuickPick for sources and targets
+      const showQuickPick = async (title: string, placeholder: string, items: string[], selectedItems: string[]): Promise<string[] | undefined> => {
+        const result = await vscode.window.showQuickPick(
+          items.map(item => ({
+            label: item,
+            picked: selectedItems.includes(item)
+          })),
+          {
+            canPickMany: true,
+            placeHolder: placeholder,
+            title: title
+          }
+        );
+        if (result === undefined) {
+          return undefined;
+        }
+        return result.map(item => item.label);
+      };
+
+      // Show QuickPick for sources
+      const selectedSources = await showQuickPick(
+        'Select Source Technologies',
+        'Choose one or more source technologies',
+        sourceOptions,
+        currentSources
+      );
+      if (selectedSources === undefined) {
+        return;
+      }
+      state.sources = selectedSources;
+
+      // Show QuickPick for targets
+      const selectedTargets = await showQuickPick(
+        'Select Target Technologies',
+        'Choose one or more target technologies',
+        targetOptions,
+        currentTargets
+      );
+      if (selectedTargets === undefined) {
+        return;
+      }
+      state.targets = selectedTargets;
+
+      // Compute initial label selector
+      const sources = state.sources.map(source => `konveyor.io/source=${source}`).join(' || ');
+      const targets = state.targets.map(target => `konveyor.io/target=${target}`).join(' || ');
+      if (sources === "" && targets === "") {
+        vscode.window.showInformationMessage("No sources or targets selected.");
+        return;
+      }
+
+      state.labelSelector = `(${[sources, targets].filter(part => part !== "").join(' && ')}) || (discovery)`;
+
+      // Show input box for modifying label selector
+      const modifiedLabelSelector = await vscode.window.showInputBox({
+        prompt: 'Modify the label selector if needed',
+        value: state.labelSelector,
+        placeHolder: 'e.g., source=(java|spring) target=(quarkus)'
+      });
+
+      if (modifiedLabelSelector === undefined) {
+        return;
+      }
+      state.labelSelector = modifiedLabelSelector;
+
+      // Update the user settings
+      await config.update('labelSelector', state.labelSelector, vscode.ConfigurationTarget.Workspace);
+
+      vscode.window.showInformationMessage(`Configuration updated: Sources: ${state.sources.join(', ')}, Targets: ${state.targets.join(', ')}, Label Selector: ${state.labelSelector}`);
+    },
+    "konveyor.configureLabelSelector": async () => {
+      const config = vscode.workspace.getConfiguration('konveyor');
+      const currentLabelSelector = config.get<string>('labelSelector', '');
+
+      const modifiedLabelSelector = await vscode.window.showInputBox({
+        prompt: 'Modify the label selector if needed',
+        value: currentLabelSelector,
+        placeHolder: 'e.g., source=(java|spring) target=(quarkus)'
+      });
+
+      if (modifiedLabelSelector === undefined) {
+        return;
+      }
+
+      // Update the user settings
+      await config.update('labelSelector', modifiedLabelSelector, vscode.ConfigurationTarget.Workspace);
     },
   };
 };
