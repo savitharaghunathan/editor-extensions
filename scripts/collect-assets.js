b5633697305387f81fd7b452c1dcdd0f14cb7414
David Zager <dzager@redhat.com>
2026-01-09 15:12:30 -0500

    :seedling: use v0.9.0 release (#1146)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    * **Chores**
    * Updated the version of release assets and seed rulesets being used by
    the CLI configuration.
    
    <sub>✏️ Tip: You can customize this high-level summary in your review
    settings.</sub>
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    Signed-off-by: David Zager <david.j.zager@gmail.com>

--- scripts/collect-assets.js
+++ scripts/collect-assets.js
@@ -17,7 +17,7 @@ const cli = parseCli(
     workflow: "build-and-push-binaries.yml",
     rulesetOrg: "konveyor",
     rulesetRepo: "rulesets",
-    releaseTag: "v0.8.0-beta.5",
+    releaseTag: "v0.9.0-alpha.4",
   },
   "release",
 );
